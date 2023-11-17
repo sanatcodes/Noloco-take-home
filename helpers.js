1290601c00b0d9bf661e523c09cb5f7e492e2c39
sanatcodes <sanat.thukral@sap.com>
2023-11-17 16:32:16 +0000

    feat: schema now vlaidates data type using multiple values

--- helpers.js
+++ helpers.js
@@ -17,66 +17,79 @@ function cleanedName(columnName) {
 
 function generateSchema(data, maxRows) {
   let schema = {};
-  let rowCount = 0;
+  let typeCounts = {}; // Object to keep track of type counts for each column
 
-  for (const row of data) {
-    if (rowCount >= maxRows) break; // Stop if upper limit is reached
+  for (let i = 0; i < Math.min(maxRows, data.length); i++) {
+    const row = data[i];
 
     for (const [key, value] of Object.entries(row)) {
-      if (!schema[key] || schema[key].type === "NULL") {
-        schema[key] = generateFieldObject(key, value, data);
+      const dataType = determineDataType(value, key, data);
+
+      if (!typeCounts[key]) {
+        typeCounts[key] = {};
+      }
+
+      if (!typeCounts[key][dataType]) {
+        typeCounts[key][dataType] = 0;
+      }
+
+      typeCounts[key][dataType] += 1;
+
+      // Update schema for the first row or if type is 'NULL'
+      if (i === 0 || !schema[key] || schema[key].type === "NULL") {
+        schema[key] = generateFieldObject(key, dataType, []);
       }
     }
+  }
 
-    rowCount++;
+  // Determine the most common type for each column
+  for (const key in typeCounts) {
+    const mostCommonType = Object.keys(typeCounts[key]).reduce((a, b) =>
+      typeCounts[key][a] > typeCounts[key][b] ? a : b
+    );
+    schema[key].type = mostCommonType;
+    schema[key].options =
+      mostCommonType === "OPTIONS"
+        ? Array.from(new Set(data.map((row) => row[key])))
+        : [];
   }
 
   return Object.values(schema); // Convert the schema object to an array of field objects
 }
 
-function generateFieldObject(columnName, columnValue, data) {
+function generateFieldObject(columnName, dataType, potentialOptions) {
   let camelCaseName = cleanedName(columnName);
-  let potentialOptions = [];
-
-  // Determine the data type
-  function determineDataType(value) {
-    if (value === null) return "NULL";
-    if (typeof value === "boolean") return "BOOLEAN";
-    if (typeof value === "number") return "INTEGER";
-    if (typeof value === "string") {
-      value = value.trim(); // Trim whitespace
-      if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/.test(value)) return "DATE";
-      if (["true", "false", "yes", "no"].includes(value.toLowerCase()))
-        return "BOOLEAN";
-
-      const isInteger = /^[+-]?\d+$/.test(value);
-      const isFloat = /^[+-]?\d+(\.\d+)?$/.test(value);
-
-      if (isInteger) return "INTEGER";
-      if (isFloat) return "FLOAT";
-
-      const values = data.map((row) => row[columnName]);
-      const unqiueValues = new Set(values);
-      const optionsThresh = 20;
-
-      if (unqiueValues.size <= optionsThresh) {
-        potentialOptions = Array.from(unqiueValues);
-        return "OPTIONS";
-      }
-    }
-
-    return "TEXT";
-  }
-
-  let dataType = determineDataType(columnValue);
 
   // Generate the field object
   return {
     display: columnName,
     name: camelCaseName,
     type: dataType,
-    options: potentialOptions.length > 0 ? potentialOptions : [],
+    options: potentialOptions,
   };
+}
+
+function determineDataType(value, columnName, data) {
+  if (value === null) return "NULL";
+  if (typeof value === "boolean") return "BOOLEAN";
+  if (typeof value === "number")
+    return Number.isInteger(value) ? "INTEGER" : "FLOAT";
+  if (typeof value === "string") {
+    value = value.trim(); // Trim whitespace
+    if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/.test(value)) return "DATE";
+    if (["true", "false", "yes", "no"].includes(value.toLowerCase()))
+      return "BOOLEAN";
+    if (/^[+-]?\d+$/.test(value)) return "INTEGER";
+    if (/^[+-]?\d+(\.\d+)?$/.test(value)) return "FLOAT";
+
+    // Analyzing for OPTION type based on unique values in the column
+    const uniqueValues = new Set(data.map((row) => row[columnName]));
+    const optionsThresh = 20;
+    if (uniqueValues.size <= optionsThresh && uniqueValues.size > 1)
+      return "OPTIONS";
+  }
+
+  return "TEXT";
 }
 
 // Function to standardize data
